fab5f7ae375602da1a8ec9a7dae069310e17f9b0
Raj Vardhan <rajvardaan130@gmail.com>
2025-02-17 20:00:10 +0530

    dice game complete withourt winner announcement

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -1,3 +1,8 @@
+let s1 = 0,
+  s2 = 0;
+
+let sb2 = Number(document.getElementById("p2sc").innerText);
+
 function start() {
   const p1 = document.getElementById("p1").value.trim();
   const p2 = document.getElementById("p2").value.trim();
@@ -23,3 +28,99 @@ function restart() {
     location.reload();
   }
 }
+
+function switchPlayer(chance) {
+  if (chance === 2) {
+    document.getElementById("rolldice1").disabled = true;
+    document.getElementById("rolldice2").disabled = false;
+  } else {
+    document.getElementById("rolldice1").disabled = false;
+    document.getElementById("rolldice2").disabled = true;
+  }
+}
+
+
+function p1Play() {
+  let dice = Math.floor(Math.random() * 6 + 1);
+  s1 = Number(document.getElementById("p1sc").innerText);
+
+  switch (dice) {
+    case 1: {
+      document.getElementById("p1dice").src = "./images/1.png";
+      break;
+    }
+    case 2: {
+      document.getElementById("p1dice").src = "./images/2.png";
+      break;
+    }
+    case 3: {
+      document.getElementById("p1dice").src = "./images/3.png";
+      break;
+    }
+    case 4: {
+      document.getElementById("p1dice").src = "./images/4.png";
+      break;
+    }
+    case 5: {
+      document.getElementById("p1dice").src = "./images/5.png";
+      break;
+    }
+    case 6: {
+      document.getElementById("p1dice").src = "./images/6.png";
+      break;
+    }
+    default: {
+      console.log("Number not in range 1-6");
+    }
+  }
+
+  if (dice === 6) {
+    switchPlayer(2);
+  } else {
+    s1 = s1 + dice;
+    document.getElementById("p1sc").innerText = s1;
+  }
+}
+
+function p2Play() {
+  let dice = Math.floor(Math.random() * 6 + 1);
+  s2 = Number(document.getElementById("p2sc").innerText);
+
+  switch (dice) {
+    case 1: {
+      document.getElementById("p2dice").src = "./images/1.png";
+      break;
+    }
+    case 2: {
+      document.getElementById("p2dice").src = "./images/2.png";
+      break;
+    }
+    case 3: {
+      document.getElementById("p2dice").src = "./images/3.png";
+      break;
+    }
+    case 4: {
+      document.getElementById("p2dice").src = "./images/4.png";
+      break;
+    }
+    case 5: {
+      document.getElementById("p2dice").src = "./images/5.png";
+      break;
+    }
+    case 6: {
+      document.getElementById("p2dice").src = "./images/6.png";
+      break;
+    }
+    default: {
+      console.log("Number not in range 1-6");
+    }
+  }
+
+  if (dice === 6) {
+    switchPlayer(1);
+  } else {
+    s2 = s2 + dice;
+    document.getElementById("p2sc").innerText = s2;
+  }
+}
+
