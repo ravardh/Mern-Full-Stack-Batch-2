e16dd4df290a60f8005eab660eae13fc9c6b9f44
Raj Vardhan <rajvardaan130@gmail.com>
2025-03-27 20:44:45 +0530

    Using Map Function

--- js/task1/script1.js
+++ js/task1/script1.js
@@ -1,33 +1,30 @@
 const punchline = document.getElementById("punchline");
 const setup = document.getElementById("setup");
-const message = document.getElementById("message")
+const message = document.getElementById("message");
 
 async function getJokes() {
   try {
-    const response = await axios.get(
+    const response = await fetch(
       "https://official-joke-api.appspot.com/jokes/randoms"
     );
     console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status} : ${response.statusText}`);
+    }
     const data = await response.json();
     console.log(data);
 
+    if (!data || !data.punchline || !data.setup) {
+      throw new Error(`Error ${data.message}`);
+    }
+
     punchline.innerText = data.punchline;
     setup.innerText = data.setup;
-    message.innerText = "Joke fetched Sucessfully"
-
+    message.innerText = "Joke fetched Sucessfully";
   } catch (error) {
-      
-    message.innerText = 'Jokes Not Found';
-  }
-  finally{
+    message.innerText = error.message;
+  } finally {
     console.log("Fetch Process Complete");
-    
   }
 }
-
-// {
-//     "type": "general",
-//     "setup": "what do you call a dog that can do magic tricks?",
-//     "punchline": "a labracadabrador",
-//     "id": 200
-//   }
