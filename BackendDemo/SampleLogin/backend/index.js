111986657320fd6c6cd2b375b199b928a20febdd
Raj Vardhan <rajvardaan130@gmail.com>
2025-04-11 20:55:55 +0530

    middleware

--- BackendDemo/SampleLogin/backend/index.js
+++ BackendDemo/SampleLogin/backend/index.js
@@ -1,17 +1,25 @@
 import express from "express";
-import authRoute from "./src/router/auth.routes.js"
+import authRoute from "./src/router/auth.routes.js";
 
 const app = express();
 
+const middleware1 = (req, res, next) => {
+  console.log("I am middle ware 1");
+  next();
+};
 
-app.use("/api/auth", authRoute)
+const middleware2 = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
 
+app.use(middleware2);
 
+app.use("/api/auth", authRoute);
 
-app.get("/", (req, res) => {
+app.get("/", middleware1, (req, res) => {
   res.json({ message: "Backend is running" });
 });
-
 
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
